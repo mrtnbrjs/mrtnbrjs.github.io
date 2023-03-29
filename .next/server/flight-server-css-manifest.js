1a9de3d6882304a8113751aa96c372d2c9722cd3
mrtnbrjs <jesus.martin2093@gmail.com>
2023-03-28 17:38:09 -0700

    nextjs blog

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/page": [
+      "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/page.module.css",
+      "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/globals.css"
+    ]
+  },
+  "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/page.tsx": [
+    "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+    "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/page.module.css"
+  ],
+  "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/layout.tsx": [
+    "/Users/martinbarajascoronado/code/mrtnbrjs.github.io/src/app/globals.css"
+  ]
+}
--- about.md
+++
@@ -1,9 +0,0 @@
----
-layout: page
-title: Hola
-permalink: /about/
----
-
-Mi nombre es Martín, y soy un desarrollador web mexicano apasionado tanto Front-End como del Back-End, amo la música, películas y temas geek.
-
-Este blog, esta dedicado lo que me pasa y quiera compartir.
